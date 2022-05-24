4b5bac7990c5e75b4c852809f47cc74759c71cd9
Felipev98 <felipe_v_spd@hotmail.com>
2022-05-24 00:14:27 -0400

    add: backend strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2ef6246d239064c42f187549e20e48ff'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ab7f240f-1081-4886-a96f-cce41624575c'
+};
